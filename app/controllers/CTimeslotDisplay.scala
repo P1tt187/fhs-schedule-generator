ac22ee5a70d91defc8ba03339fa9d861392056ba
fabian <f.markert87@googlemail.com>
2014-01-28 18:05:28 +0100

    update selectboxes

--- app/controllers/CTimeslotDisplay.scala
+++ app/controllers/CTimeslotDisplay.scala
@@ -5,27 +5,26 @@ import play.api.mvc._
 import play.db.ebean.Transactional
 import models.persistence.{Weekday, Timeslot}
 import scala.collection.JavaConversions._
-import models._
 import models.fhs.pages.timeslot.MTimeslotDisplay
-import play.api.Logger
 import com.avaje.ebean.Ebean
 
 /**
  * Created by fabian on 27.01.14.
  */
-object CTimeslotDisplay extends Controller{
+object CTimeslotDisplay extends Controller {
 
-  val NAV="timeslotdisplay"
+  val NAV = "timeslotdisplay"
 
   @Transactional
   def page() = Action {
 
 
-    val timeslotDisplay= Ebean.find(classOf[Timeslot]).fetch("parent").findList().map{
-      entry=>
+    val timeslotDisplay = Ebean.find(classOf[Timeslot]).fetch("parent").findList().map {
+      entry =>
 
-        MTimeslotDisplay(entry.startHour,entry.startMinute,entry.stopHour,entry.stopMinute,entry.parent.asInstanceOf[Weekday].name,entry.parent.asInstanceOf[Weekday].index)
-    }.sortWith(_ < _ ).toList
+        MTimeslotDisplay(entry.startHour, entry.startMinute, entry.stopHour, entry.stopMinute, entry.parent.asInstanceOf[Weekday].name, entry.parent.asInstanceOf[Weekday].index)
+    }.sortWith(_ < _).toList
+
 
     Ok(views.html.timeslotdisplay("Timeslots", timeslotDisplay))
 
