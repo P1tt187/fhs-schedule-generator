ef8ecf449b79553044de20c7d0ac00464f766d73
Fabian Markert <f.markert87@googlemail.com>
2014-06-28 06:01:16 +0200

    prevent IE from stopping executing code whe logging is enabled

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,5 +4,8 @@ appendHTML = function (url, appendContainer) {
     return $.getJSON(url, function (response) {
         return $(appendContainer).empty().append(response.htmlresult);
     });
-}
+};
 
+/** prevent stupid IE behaviour */
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function () { };
